c61d9b507418882173fcf745e601613d32fdd97f
SantAnna527 <enzosantanna.garcia@gmail.com>
2026-08-20 10:12:59 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Diferenciais from "./components/Diferenciais";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <Header /> 
+      <Hero />
+      <Diferenciais />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Header(){
+    const [menuAtivo, setMenuAtivo] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuAtivo(!menuAtivo)
+    };
+
+    return(
+        <>
+            <header className="header">
+                <h1 className="logo">Pizza Fiep</h1>
+
+                <div className="menu-toggle" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <nav className={`menu ${ menuAtivo ? 'active' :''}`}>
+                    <a href="#hero" onClick={toggleMenu}>Home</a>
+                    <a href="#cardapio" onClick={toggleMenu}>Cardapio</a>
+                    <a href="#sobre" onClick={toggleMenu}>Sobre</a>
+                    <a href="#contato" onClick={toggleMenu}>Contato</a>
+                </nav>
+            </header>
+            <div className={`overlay-menu ${menuAtivo ? 'active': ''}`} onClick={toggleMenu}>
+
+            </div>
+        </>
+    )
+}
